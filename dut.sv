f65a29edfecbbc814a34c33d59cd451e5f2dbe88
adinavale <68146367+adinavale@users.noreply.github.com>
2022-11-09 16:38:56 -0800

    flag variables are now counters

--- dut.sv
+++ dut.sv
@@ -30,70 +30,70 @@ always @ (ix.state) begin
         assert (old_state == 0 || old_state == 3 || old_state == 5) 
         else begin
           $display("Error: Legal transition to state 1 can only occur from old_state 0, 3, or 5");
-          illegal_trans_1 = 1;
+          illegal_trans_1++;
         end
       end
     2: begin
         assert (old_state == 1 || old_state == 8)
         else begin
           $display("Error: Legal transition to state 2 can only occur from old_state 1 or 8");
-          illegal_trans_2 = 1;
+          illegal_trans_2++;
         end
     end
     3: begin
         assert (old_state == 2)
         else begin
           $display("Error: Legal transition to 3 can only occur from old_state 2");
-          illegal_trans_3 = 1;
+          illegal_trans_3++;
         end
     end
     4: begin
         assert (old_state == 1 || old_state == 8)
         else begin
           $display("Error: Legal transition to 4 can only occur from old_state 1 or 8");
-          illegal_trans_4 = 1;
+          illegal_trans_4++;
         end
     end
     5: begin
         assert (old_state == 3 || old_state == 4)
         else begin
           $display("Error: Legal transition to 5 can only occur from old_state 3 or 4");
-          illegal_trans_5 = 1;
+          illegal_trans_5++;
         end
     end
     6: begin
         assert (old_state == 5)
         else begin
           $display("Error: Legal transition to 6 can only occur from old_state 5");
-          illegal_trans_6 = 1;
+          illegal_trans_6++;
         end
     end
     7: begin
         assert (old_state == 6)
         else begin
           $display("Error: Legal transition to 7 can only occur from old_state 6");
-          illegal_trans_7 = 1;
+          illegal_trans_7++;
         end
     end
     8: begin
         assert (old_state == 7 || old_state == 9)
         else begin
           $display("Error: Legal transition to 8 can only occur from old_state 7 or 9");
-          illegal_trans_8 = 1;
+          illegal_trans_8++;
         end
     end
     9: begin
         assert (old_state == 8)
         else begin
           $display("Error: Legal transition to 9 can only occue from old_state 8");
-          illegal_trans_9 = 1;
+          illegal_trans_9++;
         end
     end
     10: begin
         assert(old_state == 8)
         else begin
           $display("Error: Legal transition to 10 can only occur from old_state 8");
-          illegal_trans_10 = 1;
+          illegal_trans_10++;
         end
     end
   endcase
